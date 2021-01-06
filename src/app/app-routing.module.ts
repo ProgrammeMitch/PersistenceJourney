245a8dd9f8a5dbe0811c2d51d515b7ea26da94cb
ProgrammeMitch <mitchmbagha@gmail.com>
2021-01-06 11:43:39 +0100

    working routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BinaryConverterComponent } from './binary-conversion/binary-converter.component';
 import { BorderRadiusControlComponent } from './border-radius-control/border-radius-control.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 const routes: Routes = [
   { path: '', component: BinaryConverterComponent},
   { path: 'binary-converter', component: BinaryConverterComponent},
   { path: 'border-radius-control', component: BorderRadiusControlComponent},
+  { path: 'calculator', component: CalculatorComponent}
 ];
 
 @NgModule({
