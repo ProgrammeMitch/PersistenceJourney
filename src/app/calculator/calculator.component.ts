ade32b5c44e6d995a12c8251fc7cc54ad5e10432
ProgrammeMitch <mitchmbagha@gmail.com>
2021-01-28 12:44:29 +0100

    1. Style for calculator (SCSS) 2. Template for calculator (HTML) 3. TypeScript for calculator (TS)

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,9 +7,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() { }
+  //Initialise the display, memory and operator variable
+  displayFinalAnswer = 0;
+  holder = 0;
+  operator = '';
 
   ngOnInit(): void {
+
+  }
+  //the clear button return the display to 0
+  clearButton() {
+    this.displayFinalAnswer = 0;
   }
 
+  //create function for add, minus, divide, multiply
+  addButton() {
+    this.holder = Number(this.displayFinalAnswer);
+    this.displayFinalAnswer = 0;
+    this.operator = '+';
+  }
+  minusButton() {
+    this.holder = Number(this.displayFinalAnswer);
+    this.displayFinalAnswer = 0;
+    this.operator = '-';
+  }
+  multiplyButton() {
+    this.holder = Number(this.displayFinalAnswer);
+    this.displayFinalAnswer = 0;
+    this.operator = '*';
+  }
+  divideButton() {
+    this.holder = Number(this.displayFinalAnswer);
+    this.displayFinalAnswer = 0;
+    this.operator = '/';
+  }
+
+  //write function that displays numbers
+  button(evt: any) {
+    if (this.displayFinalAnswer > 0) {
+      this.displayFinalAnswer = this.displayFinalAnswer + evt.target.innerHTML;
+    } else if (this.displayFinalAnswer.toString().includes('.')) {
+      this.displayFinalAnswer = this.displayFinalAnswer + evt.target.innerHTML;
+    } else {
+      this.displayFinalAnswer = evt.target.innerHTML;
+    }
+  }
+
+  //create decimal function
+  decimalButton(evt: any) {
+    if (!(this.displayFinalAnswer.toString().includes('.'))) {
+      this.displayFinalAnswer = this.displayFinalAnswer + evt.target.innerHTML;
+    } else if (this.displayFinalAnswer.toString().includes('.')) {
+      this.displayFinalAnswer = this.displayFinalAnswer + 0;
+    }
+  }
+
+  //create equals function
+  equalsButton() {
+    if (this.operator === '+') {
+      let total = Number(this.holder) + Number(this.displayFinalAnswer);
+      this.displayFinalAnswer = total;
+    } else if (this.operator === '-') {
+      let total = Number(this.holder) - Number(this.displayFinalAnswer);
+      this.displayFinalAnswer = total;
+    } else if (this.operator === '*') {
+      let total = Number(this.holder) * Number(this.displayFinalAnswer);
+      this.displayFinalAnswer = total;
+    } else if (this.operator === '/') {
+      let total = Number(this.holder) / Number(this.displayFinalAnswer);
+      this.displayFinalAnswer = total;
+    }
+  }
 }
